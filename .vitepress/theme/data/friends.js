8ae418d3780a48dae5fef653668c4f1a0bf7121c
Honahec <honahec@gmail.com>
2025-09-30 20:17:43 +0800

    update(friends)

--- .vitepress/theme/data/friends.js
+++ .vitepress/theme/data/friends.js
@@ -5,5 +5,11 @@ export const friends = [
     description: "我自己的blog",
     link: "https://blog.honahec.cc",
   },
+  {
+    name: "Modenc",
+    avatar: "https://modenc.top/avatar.jpg",
+    description: "喜欢钻研各种新奇的东西",
+    link: "https://modenc.top",
+  }
   // 可以继续添加更多友链
 ];
