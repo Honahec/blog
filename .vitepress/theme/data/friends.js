6dda3dceb56db1d71aaf75d94f25b217e789a467
Honahec <honahec@gmail.com>
2025-10-01 15:26:07 +0800

    update(friends)

--- .vitepress/theme/data/friends.js
+++ .vitepress/theme/data/friends.js
@@ -10,6 +10,12 @@ export const friends = [
     avatar: "https://modenc.top/avatar.jpg",
     description: "喜欢钻研各种新奇的东西",
     link: "https://modenc.top",
+  },
+  {
+    name: "异想之旅",
+    avatar: "https://blog.yxzl.dev/head.png",
+    description: "异想之旅のBlog",
+    link: "https://blog.yxzl.dev/"
   }
   // 可以继续添加更多友链
 ];
